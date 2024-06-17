4c76a26b2a601c49a61d60c7df908ed41a8afd8c
TheOBook <o.benkreira.h@gmail.com>
2024-06-17 13:05:00 -0400

    Navbar

--- app/(main)/(routes)/documents/_components/navigation.tsx
+++ app/(main)/(routes)/documents/_components/navigation.tsx
@@ -12,7 +12,7 @@ import {
 import { cn } from "@/lib/utils";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
@@ -29,10 +29,12 @@ import {
 import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { TrashBox } from "./trash-box";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const search = useSearch();
+  const params = useParams();
   const settings = useSettings();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -184,15 +186,19 @@ export const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon
-              role="button"
-              onClick={resetWidth}
-              className="h-6 w-6 text-muted-foreground"
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon
+                role="button"
+                onClick={resetWidth}
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
