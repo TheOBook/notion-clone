4f1f194956b770f295f21cca32bd2362ff3710d2
TheOBook <o.benkreira.h@gmail.com>
2024-06-17 13:31:25 -0400

    Banner component

--- app/(main)/(routes)/documents/_components/navbar.tsx
+++ app/(main)/(routes)/documents/_components/navbar.tsx
@@ -7,6 +7,8 @@ import { MenuIcon } from "lucide-react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Title } from "./title";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -40,8 +42,12 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
